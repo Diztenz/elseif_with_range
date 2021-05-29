6cae5b688fa8ad346cd608ac8aa1a1a8370591cd
Diztenz <cosmoc05@yahoo.com>
2021-05-29 15:29:30 -0700

    Initial Commit

---
+++ elseif_with_range.py
@@ -0,0 +1,12 @@
+# Write your code here :-)
+x = 10
+for i in range(0, 20):
+    if i < x:
+        print('Too Low')
+    elif i == x:
+        print('Yayyy')
+    elif i > x:
+        print('Too High')
+
+
+
